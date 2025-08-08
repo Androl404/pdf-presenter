3226ffc716ad9eafdb171e714c85a0359af0c901
Andrei Zeucianu <benjaminpotron@gmail.com>
2025-08-08 20:57:46 +0200

    refactor: split application pointer in header file

--- src/main.c
+++ src/main.c
@@ -1,18 +1,20 @@
 #include <gtk/gtk.h>
 #include <poppler.h>
 
+#include "main.h"
 #include "pdf.h"
 #include "ui.h"
 
 PopplerDocument *document = NULL;
 PDF_data pdf_data = {0};
+GtkApplication* app;
 
 int main(int argc, char **argv) {
     // Defer PDF loading (in the case there is a PDF file from cmd arg)
     defer_pdf_loading(argc, argv);
 
     // Initialize GTK and run the application
-    GtkApplication *app = gtk_application_new("com.github.pdfviewer", G_APPLICATION_DEFAULT_FLAGS);
+    app = gtk_application_new("com.github.pdfviewer", G_APPLICATION_DEFAULT_FLAGS);
     g_signal_connect(app, "activate", G_CALLBACK(on_activate), NULL);
     int status = g_application_run(G_APPLICATION(app), 1, argv);
 
